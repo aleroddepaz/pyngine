ad52afcf792b2d9223c8ed26bb6691cdb01b7982
Alejandro <alexrdp90@gmail.com>
2012-12-10 22:29:39 +0100

    Added classifiers to setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,19 @@
 from distutils.core import setup
 
 setup(
-      name = 'PyNgine',
-      version = '0.0.1',
-      license = 'GPLv3',
-      description = 'Minimalist 3D game engine',
-      author = 'Alejandro Rodas',
-      packages = ['pyngine']
-      )
+    name = 'PyNgine',
+    version = '0.0.1',
+    license = 'GPLv3',
+    description = 'Minimalist 3D game engine',
+    author = 'Alejandro Rodas',
+    packages = ['pyngine'],
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Games/Entertainment'
+    ],
+)
